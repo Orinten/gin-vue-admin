4453a7cab9614d9013e8d4a7d138820903d8c641
Orinten <1595583713@qq.com>
2022-11-13 13:34:16 +0800

    1.初始化配置
    Signed-off-by: Orinten <1595583713@qq.com>

--- server/core/server.go
+++ server/core/server.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: Orinten 1595583713@qq.com
+ * @Date: 2022-11-13 10:57:15
+ * @LastEditors: Orinten 1595583713@qq.com
+ * @LastEditTime: 2022-11-13 13:27:03
+ * @FilePath: \server\core\server.go
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package core
 
 import (
